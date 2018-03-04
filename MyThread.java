1d6d99e63abdb1f92851000084dfeb66cd9d61c3
liiyanamegat <34502163+liiyanamegat@users.noreply.github.com>
2018-03-04 12:35:25 +0800

    Add files via upload

--- MyThread.java
+++ MyThread.java
@@ -1,23 +1,20 @@
-package mythread;
 
-class MyThread extends Thread {
+package mysleep;
 
+import static java.lang.Thread.sleep;
+
+public class MyThread extends Thread{
+    
     public void run() {
-        System.out.println("running...");
-        System.out.println("Hello Yana");
-    }
-
-    public static void main(String args[]) {
-        MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
-        System.out.println("Name of t1 : " + t1.getName());
-        System.out.println("Name of t2 : " + t2.getName());
-        System.out.println("id of t1 : " + t1.getId());
-
-        t1.start();
-        t2.start();
-
-        t1.setName("STIW3054");
-        System.out.println("After changing name of t1 : " + t1.getName());
+        System.out.println(Thread.currentThread().getName());
+        try {
+            for (int x = 0; x <= 10; x++) {
+                System.out.println(x);
+                sleep(500);
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
