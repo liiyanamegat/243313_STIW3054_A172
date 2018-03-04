94edb000979ae53f75b9c32a77f1f753e14484af
liiyanamegat <34502163+liiyanamegat@users.noreply.github.com>
2018-03-04 12:34:18 +0800

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -2,8 +2,9 @@
 package mysleep;
 
 public class MyTest extends Thread{
-    
+        
         public static void main(String[] args) {
+            System.out.println(Thread.currentThread().getName());
             MyThread t1 = new MyThread();
             t1.start();
             MyThreadThread t2 = new MyThreadThread();
