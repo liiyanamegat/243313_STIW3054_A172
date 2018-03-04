d2c9c3fca245f5b2a0bf874d1bef25439131cb40
liiyanamegat <34502163+liiyanamegat@users.noreply.github.com>
2018-03-04 12:17:04 +0800

    Add files via upload

---
+++ MyTest.java
@@ -0,0 +1,14 @@
+
+package mysleep;
+
+public class MyTest extends Thread{
+    
+        public static void main(String[] args) {
+            MyThread t1 = new MyThread();
+            t1.start();
+            MyThreadThread t2 = new MyThreadThread();
+            t2.start();
+           
+    }
+
+}
